91213502b5f9387bba09b0e16b8bb861c2afc29f
Rahul Kumar <155035402+RAHULKUMAR092@users.noreply.github.com>
2024-05-23 08:30:23 +0530

    in python

---
+++ looplist.py
@@ -0,0 +1,20 @@
+# num1 =23
+# subjectMark1 = 23
+# subjectMark2 = 55
+# subjectMark3 = 45
+# subjectMark4 = 64
+# subjectMark5 = 78
+# print(subjectMark1)
+
+#list
+subjectMarks = [23,45,65,75]
+
+# print("0 - ", subjectMarks[0])
+# print("0 - ", subjectMarks[1])
+# print("0 - ", subjectMarks[2])
+# print("0 - ", subjectMarks[3])
+
+i=0
+while i<5:
+    print(i," - ", subjectMarks[i])
+    i=i+1
